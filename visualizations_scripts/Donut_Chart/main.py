6ef504b09c7f68686e430d492e966cca123c5291
petrannce <pndirangu788@gmail.com>
2025-04-27 22:03:43 +0300

    donut chart update

--- visualizations_scripts/Donut_Chart/main.py
+++ visualizations_scripts/Donut_Chart/main.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
-sizes = [30, 20, 50]
-labels = ['A', 'B', 'C']
+sizes = [30, 20, 50, 10]
+labels = ['A', 'B', 'C', 'D']
 
-plt.pie(sizes, labels=labels, autopct='%1.1f%%', 
-        wedgeprops = {'width': 0.3})
+plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+circle = plt.Circle((0, 0), 0.70, color='white')
+plt.gca().add_artist(circle)
+
+plt.title('Donut Chart ')
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 plt.show()
